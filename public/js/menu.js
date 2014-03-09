b5b8e9d873eb2ae5f11581c3c90f07dc08cfde10
mcoppola <mcoppola832@gmail.com>
2014-03-08 23:25:43 -0500

    added back/forward buttons (hidden)

--- public/js/menu.js
+++ public/js/menu.js
@@ -8,7 +8,6 @@ $( document ).ready(function() {
   	$('.circle').css("-webkit-transform", "scale(2.9)");
       timer = setTimeout(function() {
          $('.sub-button').css('visibility', 'visible');
-         $('.sub-button').css('opacity', '.8');
       }, delay);
   }, function() {
       clearTimeout(timer);
@@ -19,10 +18,11 @@ $( document ).ready(function() {
   	$(this).find('i').css('color', '#00FFCC');
     $(this).find('i').css('text-shadow', '1px 1px #333');
   },function() {
-  	$(this).css('opacity', '.8');
     $(this).find('i').css('color', '#333');
     $(this).find('i').css('text-shadow', 'none');
+    $(this).css('opacity', '.8');
   });
+
 
   $('.menu-container').mouseleave(function() {
      $('.circle').css("-webkit-transform", "scale(1)");
@@ -32,15 +32,16 @@ $( document ).ready(function() {
       }, delay);
      timer = setTimeout(function() {
      		$('.sub-button').css('visibility', 'hidden');
-      }, delay + 300);
+        $('.sub-button').css('opacity', '0.8');
+      }, delay + 100);
   });
 
   $('.sub-button.search').hover(function() {
     $(this).find('.circle-sub').css("opacity", "1");
-    $(this).find('.circle-sub').css("-webkit-transform", "scale(2.9)");
+    $(this).find('.circle-sub').css("-webkit-transform", "scale(2.3)");
   	timer = setTimeout(function() {
-  		//document.location.href = 'https://www.google.com';
-  	}, delay + 1500);
+  		document.location.href = 'https://www.google.com';
+  	}, delay + 700);
   }, function() {
   	clearTimeout(timer);
     $(this).find('.circle-sub').css("-webkit-transform", "scale(1)");
@@ -49,7 +50,7 @@ $( document ).ready(function() {
 
   $('.sub-button.new').hover(function() {
     $(this).find('.circle-sub').css("opacity", "1");
-    $(this).find('.circle-sub').css("-webkit-transform", "scale(2.9)");
+    $(this).find('.circle-sub').css("-webkit-transform", "scale(2.3)");
   	timer = setTimeout(function() {
   		//OpenInNewTab('www.google.com');
   	}, delay + 1500);
@@ -61,7 +62,7 @@ $( document ).ready(function() {
 
   $('.sub-button.star').hover(function() {
     $(this).find('.circle-sub').css("opacity", "1");
-    $(this).find('.circle-sub').css("-webkit-transform", "scale(2.9)");
+    $(this).find('.circle-sub').css("-webkit-transform", "scale(2.3)");
     timer = setTimeout(function() {
       //do nothing for now
     }, delay + 1500);
