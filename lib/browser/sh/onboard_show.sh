36da28832866bee65675ed12708f3992850dabd7
mcoppola <mcoppola832@gmail.com>
2014-05-29 15:17:35 -0400

    changed ports

--- lib/browser/sh/onboard_show.sh
+++ lib/browser/sh/onboard_show.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
 
-
 dbus-send --type=method_call --dest=org.onboard.Onboard /org/onboard/Onboard/Keyboard org.onboard.Onboard.Keyboard.Show 
-xinput set-mode 9 ABSOLUTE
+xinput set-mode 7 ABSOLUTE
