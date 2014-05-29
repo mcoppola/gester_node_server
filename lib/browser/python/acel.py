cc4a1be7e53a9bdb3df59c37b0fa9e5b0a675f75
mcoppola <mcoppola832@gmail.com>
2014-05-28 22:46:24 -0400

    or idiot

--- lib/browser/python/acel.py
+++ lib/browser/python/acel.py
@@ -5,7 +5,7 @@ time.sleep(2) # wait
 arduino = serial.Serial('/dev/ttyACM0', 9600)
 while 1:
 	sensorValue = arduino.readline()
-	if (("yUp" in sensorValue) || ("yDn" in sensorValue)):
+	if (("yUp" in sensorValue) or ("yDn" in sensorValue)):
 		if (not keyboard):
 			print "keyboard open"
 			sys.stdout.flush()
