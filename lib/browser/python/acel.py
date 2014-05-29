4134fecacede98b58a8da0220a8adedc9aa29f79
mcoppola <mcoppola832@gmail.com>
2014-05-28 22:38:33 -0400

    sending ardunio keyboard signal

--- lib/browser/python/acel.py
+++ lib/browser/python/acel.py
@@ -1,8 +1,16 @@
 import serial, time, sys
 
+keyboard = False
 time.sleep(2) # wait
 arduino = serial.Serial('/dev/ttyACM0', 9600)
 while 1:
 	sensorValue = arduino.readline()
+	if (("yUp" in sensorValue) || ("yDn" in sensorValue)):
+		if (not keyboard):
+			arduino.write("1")
+			keyboard = True
+		else 
+			arduino.write("0")
+			keyboard = False
 	print sensorValue
 	sys.stdout.flush()
--- lib/browser/sh/onboard_show.sh
+++ lib/browser/sh/onboard_show.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 
+
 dbus-send --type=method_call --dest=org.onboard.Onboard /org/onboard/Onboard/Keyboard org.onboard.Onboard.Keyboard.Show 
 xinput set-mode 9 ABSOLUTE
